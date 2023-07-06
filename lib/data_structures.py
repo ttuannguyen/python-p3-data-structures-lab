b0c8df9c03a65db26e6b2d88f3bccfc9cdb3622c
Tuan Nguyen <trentontuannguyen@gmail.com>
2023-07-05 21:55:39 -0400

    completed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,66 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    # list comprehension method
+    return [food["name"] for food in spicy_foods]
+    
+    # for loop method
+    # names = list()
+    # for n in spicy_foods:
+    #     names.append(n["name"])
+    # return names
+
+# get_names(spicy_foods)
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
+# get_spiciest_foods(spicy_foods)
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for food in spicy_foods:
+        name = food["name"]
+        cuisine = food["cuisine"]
+        heat_level = food["heat_level"] * "🌶"
+        print(f"{name} ({cuisine}) | Heat Level: {heat_level}")
 
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
-
-def print_spiciest_foods(spicy_foods):
-    pass
-
-def get_average_heat_level(spicy_foods):
-    pass
+# print_spicy_foods(spicy_foods)
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+new_food = {
+        'name': 'Griot',
+        'cuisine': 'Haitian',
+        'heat_level': 10,
+    }
+# print(create_spicy_food(spicy_foods, new_food))
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    return [food for food in spicy_foods if cuisine == food["cuisine"]]
+
+get_spicy_food_by_cuisine(spicy_foods, "Thai")
+
+
+def print_spiciest_foods(spicy_foods):
+    # method 1:
+    # print([food for food in spicy_foods if food["heat_level"] > 5])
+
+    # method 2:
+    result = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(result)
+    
+
+# print_spiciest_foods(spicy_foods)
+
+def get_average_heat_level(spicy_foods):
+    # method 1:
+    # new_list = ([food["heat_level"] for food in spicy_foods])
+    # avg = sum(new_list)/len(new_list)
+    # return avg
+
+    # method 2:
+    return sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods)
+
+print(get_average_heat_level(spicy_foods))
